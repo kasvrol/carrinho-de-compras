920f631e2030e85d030dfb6b9774929bba6c732c
kasvrol <dovalekarolina@gmail.com>
2022-10-30 16:06:38 -0300

    feat: projeto com maven

---
+++ src/test/java/com/example/AppTest.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void testSoma() {
+        int num1 = 1;
+        int num2 = 1;
+        assertEquals(2, num1 + num2);
+    }
+}
