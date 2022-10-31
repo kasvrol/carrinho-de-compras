e2ca2c660546f09f215483ac7612a9ee30fca52d
kasvrol <dovalekarolina@gmail.com>
2022-10-30 23:12:45 -0300

    feat: switch que lerá a opção do usuário

--- src/main/java/com/example/Mercado.java
+++ src/main/java/com/example/Mercado.java
@@ -13,6 +13,36 @@ public class Mercado {
     public static void main(String[] args) {
         produtos = new ArrayList<>();
         carrinho = new HashMap<>();
+        menu();
+    }
+
+    private static void menu() {
+        System.out.println("1 - Cadastrar");
+        System.out.println("2 - Listar");
+        System.out.println("3 - Comprar");
+        System.out.println("4 - Carrinho");
+        System.out.println("5 - Sair");
+
+        int option = input.nextInt();
+
+        switch (option) {
+            case 1:
+                cadastrarProduto();
+                break;
+            case 2:
+                listarProduto();
+                break;
+            case 3:
+                comprarProduto();
+                break;
+            case 4:
+                System.out.println("Obrigada pela preferência");
+                // System.exit();
+            default:
+                System.out.println("Opção inválida!");
+                menu();
+                break;
+        }
     }
 
 }
